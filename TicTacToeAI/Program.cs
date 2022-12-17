87ed44a3d5ef7f2e90ca8a6734a8708574929ccf
Daniel Samek <samek@spstrutnov.cz>
2022-12-17 11:40:18 +0100

    Alpha beta pruning minimax

---
+++ TicTacToeAI/Program.cs
@@ -0,0 +1,313 @@
+﻿namespace TicTacToeAI;
+public class Program
+{
+    static int MapSize = 4;
+    static bool SomeoneWins = false;
+    static int WinCount = 4;
+    static int[] players = { 1, -1 };
+    static bool AIFirstMove = true;
+    static bool PlayerStarted { get; set; }
+    static int PlayerStartX { get; set; }
+    static int PlayerStartY { get; set; }
+
+
+    public static void Main(string[] args)
+    {
+        int[,] map = new int[MapSize, MapSize];
+        map = GenerateMap(map);
+        var random = new Random();
+        bool playerPlays = random.Next(0, 100) > 50;
+        PlayerStarted = playerPlays;
+        do
+        {
+            if (playerPlays)
+            {
+                DrawMap(map);
+                var pos = Console.ReadLine();
+                if (!int.TryParse(pos[0].ToString(), out int x)) continue;
+                if (!int.TryParse(pos[1].ToString(), out int y)) continue;
+				if(map[x, y] != 0) continue;
+                PlayerStartX = x;
+                PlayerStartY = y;
+                map[x, y] = 1;
+                if (CheckWin(map, out var player))
+                {    
+                    SomeoneWins = true;
+                    continue;   
+                }
+                if (!SomethingToPlay(map))
+                {
+                    DrawMap(map);
+                    Console.WriteLine("DRAW");
+                    return;
+                }
+
+                playerPlays = false;
+            }
+            else
+            {
+                if (AIFirstMove)
+                {
+                    if (!PlayerStarted)
+                    {
+                        var X = random.Next((MapSize / 2) -1, (MapSize / 2) + 1);
+                        var Y = random.Next((MapSize / 2)-1, (MapSize / 2) + 1);
+                        map[X, Y] = -1;
+                        AIFirstMove = false;
+                        playerPlays = true;
+                        continue;
+                    }
+                    var x = PlayerStartX + random.Next(-1, 1);
+                    var y = PlayerStartY + random.Next(-1, 1);
+                    try
+                    {
+                        map[x, y] = -1;
+                    }
+                    catch {
+                        continue;
+                    }
+                    AIFirstMove = false;
+                    playerPlays = true;
+                    continue;
+                }
+                
+                AI(map);
+                if (CheckWin(map, out var player))
+                {
+                    SomeoneWins = true;
+                    continue;
+                }
+                if(!SomethingToPlay(map))
+                {
+                    DrawMap(map);
+                    Console.WriteLine("DRAW");
+                    return;
+                }
+                AIFirstMove = false;
+                playerPlays = true;
+            }
+        } while (!SomeoneWins);
+        DrawMap(map);
+        var winner = playerPlays == true ? "Player" : "AI";
+        Console.WriteLine(winner);
+    }
+
+    static int[,] GenerateMap(int[,] map)
+    {
+        for (int y = 0; y < MapSize; y++) for (int x = 0; x < MapSize; x++) map[x, y] = 0;
+        return map;
+    }
+
+    static void DrawMap(int[,] map)
+    {
+        for (int i = 0; i < MapSize; i++)
+        {
+            for (int y = 0; y < MapSize; y++)
+            {
+                if (map[y, i] != 0)
+                {
+                    var customize = map[y, i] == 1 ? "X" : "O";
+                    Console.Write($"{customize}    ");
+                    continue;
+                }
+                Console.Write($"{y}{i}   ");
+            }
+            Console.WriteLine();
+        }
+    }    
+
+    static bool CheckWin(int[,] map, out int player)
+    {
+        player = 0;
+
+        for (int p = 0; p < 2; p++)
+        {
+            for (int y = 0; y < MapSize; y++)
+            {
+                for (int x = 0; x < MapSize; x++)
+                {
+                    // check for horizontal
+                    if (x + WinCount <= MapSize)
+                    {
+                        var win = true;
+                        for (int i = 0; i < WinCount; i++)
+                        {
+                            if (map[x + i,y] != players[p])
+                            {
+                                win = false;
+                            }
+                        }
+                        if (win == true)
+                        {
+                            player = players[p];
+                            return true;
+                        }
+                    }
+                    // check for vertical
+                    if (y + WinCount <= MapSize)
+                    {
+                        var win = true;
+                        for (int i = 0; i < WinCount; i++)
+                        {
+                            if (map[x,y + i] != players[p])
+                            {
+                                win = false;
+                            }
+                        }
+                        if (win == true)
+                        {
+                            player = players[p];
+                            return true;
+                        }
+                    }
+                    // check for diagonal win
+                    if (x + WinCount < MapSize + 1 && y + WinCount - 1 < MapSize)
+                    {
+                        var win = true;
+                        for (int i = 0; i < WinCount; i++)
+                        {
+                            if (map[x + i,y + i] != players[p])
+                            {
+                                win = false;
+                            }
+                        }
+                        if (win == true)
+                        {
+                            player = players[p];
+                            return true;
+                        }
+
+                    }
+                    // check for anti diagonal
+                    if (x + WinCount < MapSize + 1 && y - WinCount >= -1)
+                    {
+                        var win = true;
+                        for (int i = 0; i < WinCount; i++)
+                        {
+                            if (map[x + i,y - i] != players[p])
+                            {
+                                win = false;
+                            }
+                        }
+                        if (win == true)
+                        {
+                            player = players[p];
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+
+    static void AI(int[,] map)
+    {
+        // AI is maximalizer
+        int maxValue = int.MinValue;
+        int bestX = 0;
+        int bestY = 0;
+        int beta = int.MaxValue;
+        int alpha = int.MinValue;
+        for (int y = 0; y < MapSize; y++)
+        {
+            for (int x = 0; x < MapSize; x++)
+            {
+                if (map[x,y] == 0)
+                {
+                    map[x, y] = -1;
+                    var value = Minimax(map, 5, false, alpha, beta);
+                    map[x, y] = 0;
+                    if (value > maxValue)
+                    {
+                        maxValue = value;
+                        bestX = x;
+                        bestY = y;
+                    }
+                }
+            }
+        }
+        map[bestX, bestY] = -1;
+    }
+    
+    static int Minimax(int[,] map, int depth, bool isMaximalizer, int alpha, int beta)
+    {
+
+        if(CheckWin(map, out int player))
+        {
+            int outValue = player == 1 ? -10 : 10;
+            return outValue * depth;
+        }
+        if(depth == 0 || !SomethingToPlay(map)) return 0;
+        // for maximalizer
+        if (isMaximalizer)
+        {
+            int maxValue = int.MinValue;
+            int possibleMaxValue = (depth -1) * 10;
+            for (int y = 0; y < MapSize; y++)
+            {
+                for (int x = 0; x < MapSize; x++)
+                {
+                    if (map[x, y] == 0)
+                    {
+                        map[x, y] = -1;
+                        var value = Minimax(map, depth-1, false, alpha, beta);
+                        map[x, y] = 0;
+                        maxValue = Math.Max(value, maxValue);
+                        alpha = Math.Max(alpha, maxValue);
+                        if (possibleMaxValue == maxValue) break;
+						// beta cut
+                        if (maxValue >= beta) break;
+                    }
+                }
+            }
+            return maxValue;
+        }
+
+        // for minimalizer - player
+        if (!isMaximalizer)
+        {
+            int maxValue = int.MaxValue;
+            int possibleMaxValue = (depth - 1) * -10;
+            for (int y = 0; y < MapSize; y++)
+            {
+                for (int x = 0; x < MapSize; x++)
+                {
+                    if (map[x, y] == 0)
+                    {
+                        map[x, y] = 1;
+                        var value = Minimax(map, depth - 1, true, alpha, beta);
+                        map[x, y] = 0;
+                        maxValue = Math.Min(value, maxValue);
+                        beta = Math.Min(maxValue, beta);
+                        if (possibleMaxValue == maxValue) break;
+                        // alpha cut
+                        if (maxValue <= alpha) break;
+                    }
+                }
+            }
+            return maxValue;
+        }
+        return 0;
+    }
+
+
+    static bool SomethingToPlay(int[,] map)
+    {
+        for (int y = 0; y < MapSize; y++) for (int x = 0; x < MapSize; x++)if(map[x, y] == 0) return true;
+        return false;
+    }
+
+
+
+    static HashSet<int[]> PositionsToCheck(int[,] map)
+    {
+        HashSet<int[]> optimalPositions = new HashSet<int[]>();
+        // Vybírat tahy přímo připojené
+
+
+
+        return optimalPositions;
+    }
+}
